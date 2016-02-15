e5a8421f6b4cf1b3a08a3c2ab7dbcf160458be8a
rachain <rachain.plug@gmail.com>
2016-02-15 18:04:53 +0700

    insert and router regis page

--- routes/regis.js
+++ routes/regis.js
@@ -5,23 +5,32 @@ var router = express.Router();
 router.post('/', function(req, res, next) {
   	var username = req.body.username;
     var password = req.body.password;
+    var btn = req.body.submit;
 
-console.log('value :'+ req.body.a);
-    // var db = req.db;
-    // var collection = db.get('users');
+    if(btn=="submit"){
+	    var db = req.db;
+	    var collection = db.get('users');
 
-    // collection.insert({
-    //     username: username,
-    //     password: password
-    // }, function(err, docs)
-    // 	if(err) {
-    // 		console.log('value :'+ req.body.a);
-    //         res.send("There was a problem adding the information to the database.");
-    //     } else {
-    //         // redirect to /users 
-    //         res.redirect('profile');
-    //     }
-    // })
+	    collection.insert({
+	        username: username,
+	        password: password
+	    }, function(err, docs) {
+	        if (err) {
+	            res.send("There was a problem adding the information to the database.");
+	        } else {
+	            // redirect to /users 
+	            res.render('profile');
+	        }
+	    })
+	}else if(btn="reset"){
+		res.render('regis');
+	}
 });
+
+
+router.get('/', function(req, res, next) {
+	res.render('regis');
+});
+
 
 module.exports = router;
