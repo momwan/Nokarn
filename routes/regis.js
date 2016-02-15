3fea1417c3b0340dc61c02e70f392908ca99817c
rachain <rachain.plug@gmail.com>
2016-02-15 15:40:42 +0700

    router profile and login page

---
+++ routes/regis.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.post('/', function(req, res, next) {
+  res.render('profile'});
+});
+
+
+router.get('/login', function(req, res, next) {
+  res.render('login');
+});
+module.exports = router;
