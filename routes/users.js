7e7084e6d52e2ad8a5f09f60660b0adfadbb0ad2
rachain <rachain.plug@gmail.com>
2016-02-15 15:40:12 +0700

    users.js  -select ข้อมูล check login

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+/* GET home page. */
+router.post('/', function(req, res, next) {
+    var db = req.db;
+    var collection = db.get('users');
+    var username = req.body.username;
+    var password = req.body.password;
+    collection.find({ username: username, password: password }, function(e, docs) {
+        if (docs.length > 0) {
+            res.render('/index');
+        } else {
+            res.render('/regis');
+        }
+       
+    });
 });
 
+
+
+router.get('/login', function(req, res, next) {
+    res.render('login');
+});
 module.exports = router;
