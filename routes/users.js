a1b2bba66b7d496099ca3eb47d0c018d20da59b8
rachain <rachain.plug@gmail.com>
2016-02-15 16:00:01 +0700

    login ได้แล้ว

--- routes/users.js
+++ routes/users.js
@@ -7,7 +7,14 @@ router.post('/', function(req, res, next) {
     var collection = db.get('users');
     var username = req.body.username;
     var password = req.body.password;
-    collection.find({ username: username, password: password }, function(e, docs) {
+    collection.find({ username: username, password: password },{}, function(e, docs) {
+
+    	console.log('U: ' + username );
+    	console.log('P: ' + password);
+
+    	if (!docs) { console.log('docs is empty');}
+    	if (docs.length > 0 ) { console.log('Docs count ' + docs.length);}
+
         if (docs.length > 0) {
             res.render('index');
         } else {
