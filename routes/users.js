e263051f424a7d5e75edc3ab13a9efa00d522ecf
rachain <rachain.plug@gmail.com>
2016-02-15 16:51:06 +0700

    comment console.log (debug)

--- routes/users.js
+++ routes/users.js
@@ -9,8 +9,8 @@ router.post('/', function(req, res, next) {
     var password = req.body.password;
     collection.find({ username: username, password: password },{}, function(e, docs) {
 
-    	console.log('U: ' + username );
-    	console.log('P: ' + password);
+    	// console.log('U: ' + username );
+    	// console.log('P: ' + password);
 
     	if (!docs) { console.log('docs is empty');}
     	if (docs.length > 0 ) { console.log('Docs count ' + docs.length);}
