690d233b5096fb18ef74b7544471f0a020215789
momwan <momwan24@gmail.com>
2016-02-15 16:51:28 +0700

    wan_insert_spec
    insert spec.js

--- routes/spec.js
+++ routes/spec.js
@@ -6,13 +6,34 @@ router.get('/', function(req, res, next) {
   res.render('spec');
 });
 
-router.post('/new', function(req, res, next) {
-    console.log('test');
-    var newSpec = {
-		nation: req.body.lblnation,
-		province: req.body.lblprovince,
-		age: req.body.lblage
-	};
+router.post('/insert', function(req, res, next) {
+var nation = req.body.lblnation;
+var province = req.body.lblprovince;
+var age = req.body.lblage;
+var db = req.db;
+var collection = db.get('spec');
+
+ collection.insert({
+        nation: nation,
+        province: province,
+        age: age
+    }, function(err, docs) {
+        if (err) {
+            res.send("Not insert data to the database.");
+        } else {
+            // redirect to /users 
+            res.send("Compress");
+            res.redirect("/spec");
+        }
+    })
 
 
-module.exports = router;
+
+
+
+
+});
+
+
+
+ module.exports = router;
