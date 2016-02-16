064582a7f0b92fbd1222697a70193d5160864441
rachain <rachain.plug@gmail.com>
2016-02-16 12:38:38 +0700

    เช็ค ค่าว่างและคอนเฟิมพาสเวิด

--- routes/regis.js
+++ routes/regis.js
@@ -5,32 +5,40 @@ var router = express.Router();
 router.post('/', function(req, res, next) {
   	var username = req.body.username;
     var password = req.body.password;
+    var confrimpassword = req.body.confrimpassword;
     var btn = req.body.submit;
 
     if(btn=="insert"){
 	    var db = req.db;
 	    var collection = db.get('users');
 
-	    if(username!='' || password!=''){
+	    if(username!='' && password!='' && confrimpassword!='' ){
 
 	    	collection.find({ username: username},{}, function(e, docs) {
-	    			console.log('Docs :' + docs.length); 
+	    			//console.log('Docs :' + docs.length); 
 			        if (docs.length > 0) {
 			            res.send('Username Duplicate');
 			            
 			        }else{
 
-			        	collection.insert({
-					        username: username,
-					        password: password
-					    }, function(err, docs) {
-					        if (err) {
-					            res.send("There was a problem adding the information to the database.");
-					        } else {
-					            // redirect to /users 
-					            res.render('profile');
-					        }
-					    })
+			        	if(password== confrimpassword){
+
+				        	collection.insert({
+						        username: username,
+						        password: password
+						    }, function(err, docs) {
+						        if (err) {
+						            res.send("There was a problem adding the information to the database.");
+						        } else {
+						            // redirect to /users 
+						            res.render('profile');
+						        }
+						    })
+
+			        	}else{
+
+			        		res.send("Password and Confrimpassword Not Match");
+			        	}
 			        }
    
 			})
