8f5969adec801c7e927ec9e4d9f547f110e870e9
Teerasej Jiraphatchandej <teerasej@gmail.com>
2016-02-16 10:09:10 +0700

    Add mongoose & excel

--- app.js
+++ app.js
@@ -14,7 +14,8 @@ var db = monk('localhost:27017/nokarn');
 var spec = require('./routes/spec');
 var regis = require('./routes/regis');
 var profile = require('./routes/profile');
-
+var mongoose = require('mongoose');
+var excel = require('excel-builder');
 var app = express();
 
 // view engine setup
