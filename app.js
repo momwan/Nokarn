aef80425c9133210c591d9e6658ed34ef8de6848
momwan <momwan24@gmail.com>
2016-02-16 10:20:43 +0700

    wan_1602_01
    test  send search_ft

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ var db = monk('localhost:27017/nokarn');
 var spec = require('./routes/spec');
 var regis = require('./routes/regis');
 var profile = require('./routes/profile');
-
+var mongoose = require('mongoose');
 var app = express();
 
 // view engine setup
